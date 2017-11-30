1b0c5a551e926c246b8e14056e4d66e59c8d33a5
Maria Sut <mariasut17@gmail.com>
2017-11-30 16:38:59 +0100

    Finish with the crud for mister cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,33 @@
 class DosesController < ApplicationController
+
+  def new
+    @cocktail= Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+
+  def create
+    @cocktail= Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      puts "Failed"
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to dose_path
+  end
+
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
 end
